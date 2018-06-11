299a4f767a5799c5478e739a28a2a5168d0b95a0
r5k5 <rahulkumarnfs@gmail.com>
2018-06-11 14:59:28 +0530

    First Commit

--- caffee_test.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append('/afs/.ir.stanford.edu/users/v/r/vramesh2/tools/caffe/python')
-
-import caffe
--- convnet/train_convnet_job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#tell grid engine to use current directory
-#$ -cwd
-
-nodejs train_convnet.js
